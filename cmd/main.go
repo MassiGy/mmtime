4756de93649cc31add29fb8af47e93b1a963c357
Massiles Ghernaout <massiles.ghernaout@renault.com>
2024-04-16 16:40:12 +0200

    beta version of the program done ( successfully able to track and monitor usage time of target tasks)

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,142 @@
-package main;
+package main
 
-import "fmt";
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"mmtime/types"
+	"mmtime/utils"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
 
 func main() {
-    fmt.Println("mmtime starting....");
-}
+	filename := "/home/massigy/.config/mmtime/targets"
 
+	// read the ~/.config/mmtime/targets file
+	file, err := os.Open(filename)
+	if err != nil {
+		// create the file
+		os.WriteFile(
+			filename,
+			[]byte("# Add the applications that you want to track your usage time in, each application in a seperated line\n"),
+			0666,
+		)
+		return
+	}
+
+	defer file.Close()
+
+	// load the tokens from the file to a slice of tasks
+	tasks := []types.Task{}
+
+	chars, err := ioutil.ReadAll(file)
+	utils.Check(err)
+
+	fileContent := string(chars)
+	tasksNames := strings.Split(fileContent, "\n")
+
+	for _, name := range tasksNames {
+		// ignore commented lines
+		if strings.HasPrefix(name, "#") {
+			continue
+		}
+
+		// ignore empty lines
+		if len(name) <= 1 {
+			continue
+		}
+
+		tasks = append(tasks, types.Task{
+			Name: name,
+		})
+	}
+
+	monitorTasks(&tasks)
+
+	// setup a ticker to listen to
+	ticker := time.NewTicker(10 * time.Second)
+
+	// create a channel to which POSIX signals will be deleivered to
+	// make it a buffered channel (manage only one signal at the time)
+	sigPause := make(chan os.Signal, 1)
+	sigContinue := make(chan os.Signal, 1)
+	sigTerm := make(chan os.Signal, 1)
+
+	// SIGSTOP can not be trapped, but we are leaving it for demo purposes
+	signal.Notify(sigPause, syscall.SIGSTOP)
+
+	signal.Notify(sigContinue, syscall.SIGCONT)
+	signal.Notify(sigTerm, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGINT)
+
+	for {
+		select {
+		case <-ticker.C:
+			{
+				monitorTasks(&tasks)
+			}
+		case <-sigPause:
+			{
+				// make the ticker.C = nil so as
+				// its case will be ignored
+				ticker.C = nil
+			}
+		case <-sigContinue:
+			{
+				// make the ticker.C valid again (!nil)
+				ticker = time.NewTicker(10 * time.Second)
+			}
+		case <-sigTerm:
+			{
+				os.Exit(0)
+			}
+		}
+	}
+}
+func monitorTasks(tasks *[]types.Task) {
+
+	shellCommand := "ps -axco command| tail -n +2|uniq"
+	shellInterpreter := "bash"
+
+	var cmdStdOut bytes.Buffer
+	var cmdStdErr bytes.Buffer
+
+	cmd := exec.Command(shellInterpreter, "-c", shellCommand)
+	cmd.Stdout = &cmdStdOut
+	cmd.Stderr = &cmdStdErr
+
+	err := cmd.Run()
+	utils.Check(err)
+
+	if len(cmdStdErr.String()) >= 1 {
+		panic(errors.New("could not fetch currently running processes"))
+	}
+
+	stdOutLines := strings.Split(cmdStdOut.String(), "\n")
+
+	var nonInitializedTimeInstant time.Time
+
+	for _, line := range stdOutLines {
+		// every line is a process name
+		for i, task := range *tasks {
+
+			if strings.Compare(strings.ToLower(line), strings.ToLower(task.Name)) == 0 {
+
+				if (*tasks)[i].LaunchedAt.Equal(nonInitializedTimeInstant) {
+					(*tasks)[i].LaunchedAt = time.Now()
+					continue
+				}
+
+				(*tasks)[i].UsedFor = time.Since((*tasks)[i].LaunchedAt)
+			}
+		}
+	}
+	for _, task := range *tasks {
+		fmt.Println(task)
+	}
+}
--- types/mmtime.go
+++ types/mmtime.go
@@ -1,1 +1,12 @@
-package types;
+package types
+
+import "time"
+
+type Task struct {
+	Name       string
+	LaunchedAt time.Time
+	UsedFor    time.Duration
+}
+
+
+
