604cde4e110415c6eaead25b9563ee5da2bf9df3
Massiles Ghernaout <massiles.ghernaout@renault.com>
2024-04-17 11:28:24 +0200

    +refectoring, +saving stats to db file, +corrections on time usage after pause/power down/interupt)

--- Makefile
+++ Makefile
@@ -12,3 +12,20 @@ run:
 
 test:
 	echo "(Makefile) tests are not setup yet"
+
+create_directories: 
+	# create the needed directories
+	# file creation is taken care of by the program itself
+	mkdir ~/.config/mmtime &&
+	mkdir ~/.local/share/mmtime &&
+
+make_bin_global: 
+	mv ./bin/mmtime /usr/local/bin 
+
+install: clean build create_directories make_bin_global
+
+launch: /usr/local/bin/mmtime 
+
+	
+
+
--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,75 @@ import (
 )
 
 func main() {
+	tasks := getTasksFromConf()
+	if len(tasks) == 0 {
+		// targets file just got created
+		return
+	}
+
+	monitorTasks(&tasks) // fst call to register tasks
+
+	// setup a ticker to listen to
+	tickCycle := 10 * time.Second
+	ticker := time.NewTicker(tickCycle)
+
+	// create a channel to which POSIX signals will be deleivered to
+	// make it a buffered channel (manage only one signal at the time)
+	sigPower := make(chan os.Signal, 1)
+	sigContinue := make(chan os.Signal, 1)
+	sigTerm := make(chan os.Signal, 1)
+
+	// SIGPWR=battery|power lvl low (shutdown|hibernation|suspend mode)
+	signal.Notify(sigPower, syscall.SIGPWR)
+
+	signal.Notify(sigContinue, syscall.SIGCONT)
+	signal.Notify(sigTerm, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGINT)
+
+	for {
+		select {
+		case <-ticker.C:
+			{
+				monitorTasks(&tasks)
+			}
+		case <-sigPower:
+			{
+				// make the ticker.C = nil so as
+				// its case will be ignored
+				ticker.C = nil
+
+				// flush the current stats to db
+				saveCurrentStats(tasks)
+
+			}
+		case <-sigContinue:
+			{
+				// make the ticker.C valid again (!nil)
+				ticker = time.NewTicker(tickCycle)
+
+				// update the tasks, this can be done after ticker re-initialization
+				// since the tickCycle is long then the update job
+				var nonInitializedTimeInstant time.Time
+				for i, task := range tasks {
+
+					// ignore non used tasks
+					if task.LaunchedAt.Equal(nonInitializedTimeInstant) {
+						continue
+					}
+
+					tasks[i].LaunchedAt = time.Now()
+				}
+
+			}
+		case <-sigTerm:
+			{
+				return
+			}
+		}
+	}
+}
+
+func getTasksFromConf() []types.Task {
+
 	filename := "/home/massigy/.config/mmtime/targets"
 
 	// open the ~/.config/mmtime/targets file
@@ -25,11 +94,11 @@ func main() {
 		// create the file and exit
 		err := os.WriteFile(
 			filename,
-			[]byte("# Add the applications that you want to track your usage time in, each application in a seperated line\n"),
+			[]byte(`# Add the applications that you want to track your usage time in, each application in a seperated line`),
 			0666,
 		)
 		utils.Check(err)
-		return
+		return nil
 	}
 
 	defer file.Close()
@@ -58,49 +127,9 @@ func main() {
 			Name: name,
 		})
 	}
-
-	monitorTasks(&tasks)
-
-	// setup a ticker to listen to
-	tickCycle := 10 * time.Second
-	ticker := time.NewTicker(tickCycle)
-
-	// create a channel to which POSIX signals will be deleivered to
-	// make it a buffered channel (manage only one signal at the time)
-	sigPower := make(chan os.Signal, 1)
-	sigContinue := make(chan os.Signal, 1)
-	sigTerm := make(chan os.Signal, 1)
-
-	// SIGPWR is sent when the battery|power level is low
-	signal.Notify(sigPower, syscall.SIGPWR)
-
-	signal.Notify(sigContinue, syscall.SIGCONT)
-	signal.Notify(sigTerm, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGINT)
-
-	for {
-		select {
-		case <-ticker.C:
-			{
-				monitorTasks(&tasks)
-			}
-		case <-sigPower:
-			{
-				// make the ticker.C = nil so as
-				// its case will be ignored
-				ticker.C = nil
-			}
-		case <-sigContinue:
-			{
-				// make the ticker.C valid again (!nil)
-				ticker = time.NewTicker(tickCycle)
-			}
-		case <-sigTerm:
-			{
-				return
-			}
-		}
-	}
+	return tasks
 }
+
 func monitorTasks(tasks *[]types.Task) {
 
 	shellCommand := "ps -axco command| tail -n +2|uniq"
@@ -145,4 +174,48 @@ func monitorTasks(tasks *[]types.Task) {
 	for _, task := range *tasks {
 		fmt.Println(task)
 	}
+}
+
+func saveCurrentStats(tasks []types.Task) {
+	// db file
+	filename := "/home/massigy/.local/share/mmtime/targets.stats.db"
+
+	// open the file for RW(do not create to add the desc comment at first)
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0600)
+
+	utils.Check(err)
+	defer file.Close()
+
+	// read all the file's content
+	chars, err := ioutil.ReadAll(file)
+	utils.Check(err)
+
+	fileContent := string(chars)
+	if len(fileContent) <= 1 {
+		// file just got created, write the dsc comment into it
+		file.WriteString("# This file stores the mmtime stats according to your time usage of the applications that you specified in ~/.config/mmtime/targets\n")
+		file.WriteString("# For every application/process just sum up the usage times for any given date to get the total usage during that day.\n")
+		file.WriteString("\n")
+		file.WriteString("Process\tDate\t\tUsage\n")
+	}
+
+	var nonInitializedTimeInstant time.Time
+
+	// write the stats to the file
+	for _, task := range tasks {
+
+		// if the current task was not used, do not write it to db file
+		if task.LaunchedAt.Equal(nonInitializedTimeInstant) {
+			continue
+		}
+
+		file.WriteString(
+			fmt.Sprintf("%s\t%v\t%v\n",
+				task.Name,
+				task.LaunchedAt.Format("2006-01-02"),
+				task.UsedFor,
+			),
+		)
+	}
+
 }
